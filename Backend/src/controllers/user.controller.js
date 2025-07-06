74d1dbf4a6d5dabbedabe98660204b0146a9541a
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2025-07-06 17:48:33 +0530

    User registration/Login Done -> Token

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,57 @@
+import httpStatus from "http-status";
+import {User} from "../models/user.model.js";
+import bcrypt , {hash} from "bcrypt";
+
+const login = async(req,res)=>{
+    const {username, password} = req.body;
+    if(!username || !password){
+        return res.status(httpStatus.BAD_REQUEST).json({message: "Please Provide..."})
+    }
+    try{
+        const user = await User.find({username});
+        if(!user){
+            return res.status(httpStatus.NOT_FOUND).json({message: "User not found..."});
+        }
+        if(bcrypt.compare(password, user.password)){
+            let token = crypto.randomBytes(20).toString("hex");
+            user.token = token;
+            await user.save();
+            return res.status(httpStatus.OK).json({token: token});
+
+        }
+    }
+    catch (e){
+        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message: `Something went wrong ${e}`});
+
+    }
+}
+
+const register = async (req,res)=>{
+    const {name, username, password } = req.body;
+    try{
+        const existingUser = await User.findOne({username});
+        if(existingUser){
+            return res.status(httpStatus.FOUND).json({message: "User already exists"});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User(
+            {
+                name: name,
+                username: username,
+                password: hashedPassword,
+            }
+        );
+        await newUser.save();
+
+        res.status(httpStatus.CREATED).json({message: "User Registered"});
+
+
+    }
+    catch (e){
+        res.json({message: `Something went wrong ${e}`});
+
+
+    }
+
+}
+export {login, register};
