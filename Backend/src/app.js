056bbef27e837167f0721277080976d95c05d795
Tr0j4n-Himanshu <tripathi.himanshu0508@gmail.com>
2025-07-07 10:17:20 +0530

    URI secured from Git

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,19 @@
+
 import express from "express";
 import {createServer} from "node:http";
 import {Server} from "socket.io";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import {connectToSocket} from "./controllers/socketManager.js";
 import userRoutes from "./routes/users.routes.js";
-
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000));
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
@@ -21,7 +24,7 @@ app.use("/api/v1/users", userRoutes);
 
 
 const start = async()=>{
-    const connectionDb = await mongoose.connect("mongodb+srv://tripathihimanshu:himanshu6905@cluster0.hbgroai.mongodb.net/")
+    const connectionDb = await mongoose.connect(process.env.MONGO_URI); //MongoURI from .env file(secured)
     console.log(`MONGO Connected DB Host: ${connectionDb.connection.host} `)
     server.listen(app.get("port"), ()=>{
         console.log("Listening to PORT 8000");
--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import httpStatus from "http-status";
 import {User} from "../models/user.model.js";
 import bcrypt , {hash} from "bcrypt";
+import crypto from "crypto";
 
 const login = async(req,res)=>{
     const {username, password} = req.body;
@@ -8,7 +9,7 @@ const login = async(req,res)=>{
         return res.status(httpStatus.BAD_REQUEST).json({message: "Please Provide..."})
     }
     try{
-        const user = await User.find({username});
+        const user = await User.findOne({username});
         if(!user){
             return res.status(httpStatus.NOT_FOUND).json({message: "User not found..."});
         }
